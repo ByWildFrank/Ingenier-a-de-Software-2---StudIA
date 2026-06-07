184bf3973bf5358d2919b85ab896c7e408407cc6
renavillalba09 <renavillalba09@gmail.com>
2026-06-07 16:42:00 -0300

    el @enduml

--- backend/src/routes/dashboard.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/dashboard.controller');
-
-router.get('/stats', controller.obtenerEstadisticas);
-
-module.exports = router;
--- backend/src/routes/respuestas.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/respuestas.controller');
-
-router.get('/flashcard/:flashcardId', controller.obtenerPorFlashcard);
-router.post('/', controller.crear);
-
-module.exports = router;
